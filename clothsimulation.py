b8707072be9f6140eebf4fd35acc4d4975260403
Gianmarco Caramitti <c.giammy95@gmail.com>
2022-12-30 22:10:05 +0100

    Update clothsimulation.py

--- clothsimulation.py
+++ clothsimulation.py
@@ -143,39 +143,30 @@ print(len(springs))
 
 pygame.init()
 
-# Set the width and height of the screen [width, height]
+
 size = (600, 1000)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Verlet Physics")
 
-# Loop until the user clicks the close button.
+
 done = False
 
-# Used to manage how fast the screen updates
+
 clock = pygame.time.Clock()
 
-# -------- Main Program Loop -----------
+
 while not done:
-    # --- Main event loop
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    # --- Game logic should go here
     physics.update()
-    # print(particles[39].x, particles[39].y)
 
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
     screen.fill(WHITE)
 
-    # --- Drawing code should go here
+
     # for row in particles:
     #    for p in row:
     #        p.display()
@@ -183,12 +174,11 @@ while not done:
     for s in springs:
         s.display()
 
-    # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
-    # --- Limit to 60 frames per second
+
     clock.tick(60)
 
-# Close the window and quit.
+
 pygame.quit()
 
